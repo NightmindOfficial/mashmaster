33e7bcbadea88b20943e712e992c5da78c9891c2
Otis Mohr <mail@otismohr.de>
2025-03-03 15:01:50 +0100

    Stage Improvements

--- lib/layout/stage.dart
+++ lib/layout/stage.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:mashmaster/models/destination.dart';
 import 'package:mashmaster/router/routes.dart';
+import 'package:mashmaster/screens/settings_screen/widgets/settings_icon_button.dart';
 
 class Stage extends StatelessWidget {
   final StatefulNavigationShell navigationShell;
@@ -13,15 +14,18 @@ class Stage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final GoRouter router = GoRouter.of(context);
+
     // Get current route path
     final String currentPath =
-        GoRouter.of(context).routeInformationProvider.value.uri.toString();
-    dev.log(currentPath);
+        router.routeInformationProvider.value.uri.toString();
+    dev.log("Current Page: $currentPath");
 
     final String title = Routes.getTitle(currentPath);
 
-    //Determine if back buttion should be visible
-    final bool canGoBack = GoRouter.of(context).canPop();
+    //Check if GoRouter can pop (for global routes like /settings)
+    final bool canPopGlobal = router.canPop();
+    dev.log("canPopGlobal -> $canPopGlobal");
 
     return Scaffold(
       ///* APPBAR
@@ -29,18 +33,15 @@ class Stage extends StatelessWidget {
         title: Text(title, style: TextStyle(fontWeight: FontWeight.bold)),
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         leading:
-            canGoBack
+            canPopGlobal
                 ? IconButton(
                   icon: Icon(Icons.arrow_back),
-                  onPressed: () => GoRouter.of(context).pop(),
+                  onPressed: () {
+                    router.pop();
+                  },
                 )
                 : null,
-        actions: [
-          IconButton(
-            onPressed: () => context.push(MainRoute.settings.pathAbs),
-            icon: const Icon(Icons.settings_rounded),
-          ),
-        ],
+        actions: [SettingsIconButton()],
       ),
 
       ///* BODY
