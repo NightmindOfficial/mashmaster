f03d219ffa736685de03b189eb119b9736ced6e1
Otis Mohr <mail@otismohr.de>
2025-03-03 11:52:08 +0100

    Implemented Init Layout for Nested Screens, will change later

--- lib/screens/home_screen/pages/home_screen_dosage_calc.dart
+++ lib/screens/home_screen/pages/home_screen_dosage_calc.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:mashmaster/helpers/home_screen_cards.dart';
 
 class HomeScreenDosageCalc extends StatelessWidget {
   const HomeScreenDosageCalc({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: SizedBox(height: 150, width: 300, child: homeScreenCards[0]),
+    );
   }
 }
--- lib/screens/home_screen/pages/home_screen_general_calc.dart
+++ lib/screens/home_screen/pages/home_screen_general_calc.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:mashmaster/helpers/home_screen_cards.dart';
 
 class HomeScreenGeneralCalc extends StatelessWidget {
   const HomeScreenGeneralCalc({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: SizedBox(height: 150, width: 300, child: homeScreenCards[1]),
+    );
   }
 }
