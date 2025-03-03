5c8c33e5e8e804e7251ace51e7b693c6d89f9a9f
Otis Mohr <mail@otismohr.de>
2025-03-03 14:10:00 +0100

    Refactored Settings IconButton

---
+++ lib/screens/settings_screen/widgets/settings_icon_button.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:mashmaster/router/routes.dart';
+
+import 'dart:developer' as dev;
+
+class SettingsIconButton extends StatelessWidget {
+  const SettingsIconButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return IconButton(
+      onPressed: () {
+        if (context.canPop()) {
+          dev.log("Route can pop. Replacing current route with new route.");
+          context.pushReplacement(MainRoute.settings.pathAbs);
+        } else {
+          dev.log("Route can NOT pop. Pushing new route.");
+          context.push(MainRoute.settings.pathAbs);
+        }
+      },
+      icon: const Icon(Icons.settings_rounded),
+    );
+  }
+}
