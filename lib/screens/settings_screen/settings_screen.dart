20f4b9af58257d22573ba5b4c8e64fb5aa074a10
Otis Mohr <mail@otismohr.de>
2025-03-17 18:26:16 +0100

    Settings Screen Refactor

--- lib/screens/settings_screen/settings_screen.dart
+++ lib/screens/settings_screen/settings_screen.dart
@@ -1,17 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:mashmaster/i18n/generated/translations.g.dart';
 import 'package:mashmaster/layout/stage_app_bar.dart';
+import 'package:mashmaster/screens/settings_screen/widgets/language_dropdown.dart';
 import 'package:mashmaster/screens/settings_screen/widgets/system_theme_switch.dart';
 import 'package:mashmaster/screens/settings_screen/widgets/theme_switch.dart';
 
-class SettingsScreen extends StatefulWidget {
+class SettingsScreen extends StatelessWidget {
   const SettingsScreen({super.key});
 
-  @override
-  State<SettingsScreen> createState() => _SettingsScreenState();
-}
-
-class _SettingsScreenState extends State<SettingsScreen> {
   @override
   Widget build(BuildContext context) {
     final t = Translations.of(context);
@@ -26,31 +22,7 @@ class _SettingsScreenState extends State<SettingsScreen> {
           padding: EdgeInsets.symmetric(vertical: 32, horizontal: 16),
           child: Column(
             children: [
-              ListTile(
-                leading: Icon(Icons.language_rounded),
-                title: Text(t.language.label),
-                titleAlignment: ListTileTitleAlignment.center,
-                trailing: DropdownMenu(
-                  initialSelection: LocaleSettings.currentLocale,
-                  onSelected: (locale) {
-                    if (locale != null) {
-                      LocaleSettings.setLocale(locale);
-                    }
-                  },
-                  dropdownMenuEntries: <DropdownMenuEntry<AppLocale>>[
-                    DropdownMenuEntry(
-                      leadingIcon: Text("🇬🇧"),
-                      value: AppLocale.en,
-                      label: t.language.en,
-                    ),
-                    DropdownMenuEntry(
-                      leadingIcon: Text("🇩🇪"),
-                      value: AppLocale.de,
-                      label: t.language.de,
-                    ),
-                  ],
-                ),
-              ),
+              LanguageDropdown(),
               Divider(height: 32),
               ThemeSwitch(),
               SystemThemeSwitch(),
